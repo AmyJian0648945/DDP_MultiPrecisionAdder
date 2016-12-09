2b91ce0b04d30f1aec9ed7756076df8a83d93ef8
AmyJian0648945 <amychien314159@gmail.com>
2016-12-09 20:53:45 +0100

    mont is correct. also updated its printing functions

--- main_RSA.c
+++ main_RSA.c
@@ -81,6 +81,7 @@ void print_bram_contents(){
     xil_printf("BRAM contents:\n\r");
     for (i=0; i<DMA_TRANSFER_NUMBER_OF_WORDS; i+=4)
         xil_printf("%08x %08x %08x %08x\n\r",my_montgomery_data[i], my_montgomery_data[i+1], my_montgomery_data[i+2], my_montgomery_data[i+3]);
+    xil_printf("\n\r");
 }
 
 
@@ -89,41 +90,45 @@ void print_bram_contents(){
 
 void mont(uint32_t *result, uint32_t *A, uint32_t *B, uint32_t *m){
 
+    xil_printf("Start Mont: ");
+
     // Send message
-    xil_printf("Sending A\n\r");
+    xil_printf("Sending A...");
     my_montgomery_port[0] = 0x0; //Send a command to P1
     bram_dma_transfer(dma_config, A, DMA_TRANSFER_NUMBER_OF_WORDS);
     port2_wait_for_done(); //Wait until Port2=1
 
 
     // Send rsqModM
-    xil_printf("Sending B\n\r");
+    xil_printf("Sending B...\n\r");
     my_montgomery_port[0] = 0x0; //Send a command to P1
     bram_dma_transfer(dma_config, B, DMA_TRANSFER_NUMBER_OF_WORDS);
     port2_wait_for_done(); //Wait until Port2=1
 
 
     // Send modullus
-    xil_printf("Sending m\n\r");
+    xil_printf("Sending m...");
     my_montgomery_port[0] = 0x0; //Send a command to P1
     bram_dma_transfer(dma_config, m, DMA_TRANSFER_NUMBER_OF_WORDS);
     port2_wait_for_done(); //Wait until Port2=1
 
 
     // Compute Mont(message, rsqModM)
-    xil_printf("Computing...\n\r");
+    xil_printf("Computing...");
     my_montgomery_port[0] = 0x1; //Send a command to P1
     port2_wait_for_done(); //Wait until Port2=1
-    print_bram_contents(); //Print BRAM to serial port.
+    //print_bram_contents(); //Print BRAM to serial port.
 
 
     // Print x_delta values
-    xil_printf("Results! \n\r");
+    /*
+    xil_printf("Results: \n\r");
     my_montgomery_port[0] = 0x2; //Send a command to P1
     port2_wait_for_done(); //Wait until Port2=1
     print_bram_contents(); //Print BRAM to serial port.
-
-    int i =0;
+    */
+    xil_printf("Done!\n\n\r");
+    int i = 0;
 
     // Give the results to 'result'
     for (i=0; i<DMA_TRANSFER_NUMBER_OF_WORDS; i+=4){
@@ -221,14 +226,29 @@ int main()
 
 
     init_platform();
-
-    xil_printf("Startup..\n\r");
-
+    xil_printf("\n\n\n\n\n\n\n\nStartup..\n\r");
     test_dma_transfer();
 
-    /////////// Initialise variables - change 'testVector.h' if necessary!
+    /////////// Initialize variables - change 'testVector.h' if necessary!
     // Seed = 2016.
     // numbers are least significant to most significant
+
+
+
+
+
+/*
+    //TESTING PURPOSES ONLY -
+
+    uint32_t a[32] = {0x1ba, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 , 0x0};
+    uint32_t b[32] = {0x91b, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    uint32_t N[32] = {0xe67a6f31, 0xa677bce5, 0x3b9e66f8, 0x32dff402, 0x2ab9a7c8, 0xbe08bc1e, 0x2ade1186, 0x8e88e8cf, 0x716a5c3b, 0x5453c47, 0x93ade446, 0xdc771f42, 0xe1709725, 0x7f89ebb5, 0x1d501ea9, 0x93c9933, 0xcf1ae468, 0x56ba1eb0, 0x358e7c24, 0x2daae084, 0x4da058ec, 0x27028a89, 0x4f2e5438, 0x29ab96b2, 0xf9072101, 0xe7990185, 0x199af8fe, 0x6887926d, 0xcfb0450e, 0x36a53dd9, 0xed3982c, 0xd1d3b4d6};
+    uint32_t result[32] = {0};
+
+    mont(result, a, b, N);
+    print_output(result);
+*/
+
 
 
 
@@ -245,26 +265,41 @@ int main()
     print_output(output_ciphertext);
 
 
-
+/*
 
     /////////// Decryption
-    numOfBits = 32;
-    decryption(rsqModM, rModM, d, numOfBits, modullus, output_ciphertext, output_message);
+    //numOfBits = 32;
+    //decryption(rsqModM, rModM, d, numOfBits, modullus, output_ciphertext, output_message);
 
 
 
     /////////// Print out decryption results
-    print_output(output_message);
+    //print_output(output_message);
 
 
 
     /////////// Compare the encrypted & decrypted results
     //output_ciphertext - output_message;
+     *
+     *
+*/
+
+    /************************
+    DEBUG TODO:
+    - check the warnings!
+    - make sure mont is correct
+        - make sure that the output of it is correct
+    - double check that the mont exponentiation is following the correct flow:
+        -
+    -
+    - make sure that the array is processing through the info in the correct order
 
 
-    // TODO:
-    // 1) make sure that the array is processing through the info in the correct order
 
+
+
+
+    ************************/
 
 
 
